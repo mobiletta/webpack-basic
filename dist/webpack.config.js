387a458758526f9d0937533abcb38e1c574c9690
Brian Weber <brian@mobiletta.com>
2017-08-11 18:20:31 -0500

    always something

---
+++ dist/webpack.config.js
@@ -0,0 +1,57 @@
+// webpack.config.js
+const webpack = require('webpack')
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const extractCSS = new ExtractTextPlugin('[name].bundle.css')
+
+const extractCommons = new webpack.optimize.CommonsChunkPlugin({
+  name: 'commons',
+  filename: 'commons.js'
+})
+
+
+const config = {
+  context: path.resolve(__dirname, 'src'),
+	entry: {
+    app: './app.js',
+    admin: './admin.js'
+  },
+ output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      include: path.resolve(__dirname, 'src'),
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['es2015', { modules: false }]
+          ]
+        }
+      }]
+    },
+    {
+		  test: /\.scss$/,
+		  loader: ['style-loader', 'css-loader','sass-loader'])
+      //loader: extractCSS.extract(['css-loader','sass-loader'])
+		},
+		{
+		  test: /\.(png|jpg)$/,
+		  use: [{
+		    loader: 'url-loader',
+		    options: { limit: 10000 } // Convert images < 10k to base64 strings
+  		}]
+  	}]
+  },
+  plugins: [
+  	new webpack.NamedModulesPlugin(),
+    extractCSS,
+    extractCommons
+  ]
+}
+
+module.exports = config
